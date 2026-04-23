77cf271606049a50b2cc243a0098852dba05b0ee
Kalpana-Chaurasiya21 <kalpanaachaurasiya88@gmail.com>
2026-04-23 23:32:10 +0530

    Add files via upload

---
+++ input_output.py
@@ -0,0 +1,32 @@
+#User input code
+name=input("Enter your name: ")
+print("Hello,", name," !Welcome")
+s="Kalpana"
+print(s)
+
+age=21
+city="New York"
+print(s,age,city)
+#split
+x,y=input("Enter two numbers: ").split()
+print(x,y)
+
+#integer input
+
+age = int(input("Enter your age: "))
+print("Your age is:", age)
+
+#Taking two numbers and adding them
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Sum =", a+b)
+
+#User details 
+name = input("Enter name: ")
+age = int(input("Enter age: "))
+city = input("Enter city: ")
+
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
