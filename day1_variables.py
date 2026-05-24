8118c1c04019025114e788419f01f5e68fcc043b
Kalpana-Chaurasiya21 <kalpanaachaurasiya88@gmail.com>
2026-05-24 20:44:03 +0530

    Add files via upload

---
+++ day1_variables.py
@@ -0,0 +1,40 @@
+#variables are used to store data in python
+#we can store numbers, text and other values
+
+#creating variables
+
+a = 10
+b = 3.5
+name = "kalpana"
+is_student = True
+
+print(a)
+print(b)
+print(name)
+print(is_student)
+
+
+#checking data types
+
+print(type(a))        # output: <class 'int'>
+print(type(b))        # output: <class 'float'>
+print(type(name))     # output: <class 'str'>
+print(type(is_student))   # output: <class 'bool'>
+
+
+#multiple variables
+
+x, y, z = 1, 2, 3
+
+print(x)
+print(y)
+print(z)
+
+
+#same value to multiple variables
+
+a = b = c = 5
+
+print(a)
+print(b)
+print(c)
